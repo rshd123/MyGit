c2409fc2f5b32844c874c4c5af57f046d10d37aa
M.Arshad <arshadm1512@gmail.com>
2025-03-01 19:56:42 +0530

    deploy config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
+    strictPort: true,
+    allowedHosts: ['mygit-8hme.onrender.com'], // Add your Render domain here
+  },
 })
