cffbcd8dd65a83ab87f9602bcfe80432aedd20a7
M.Arshad <arshadm1512@gmail.com>
2025-03-01 22:20:54 +0530

    deploy config

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -2,7 +2,7 @@ import React ,{useEffect,useState}from "react";
 import { useAuth } from "../../AuthContext.jsx";
 import axios from "axios";
 // import dotenv from "dotenv";
-dotenv.config();
+// dotenv.config();
 import { PageHeader } from "@primer/react";
 import { Box, Button } from "@primer/react";
 
